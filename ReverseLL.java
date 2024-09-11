545944e9d5290debaa4366adaa763327b4b22fe7
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-09-12 00:37:23 +0530

    Reverse LinkedList

---
+++ ReverseLL.java
@@ -0,0 +1,16 @@
+class ReverseLL {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode frwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = frwd;
+        }
+        return prev;
+    }
+}
