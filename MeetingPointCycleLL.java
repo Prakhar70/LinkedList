4e42b4a93bd3719830b77fe0ca1cdbb8b321b64a
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-09-13 22:11:11 +0530

    Start Point of cycle

---
+++ MeetingPointCycleLL.java
@@ -0,0 +1,28 @@
+public class MeetingPointCycleLL {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null){
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean isCycle = false;
+        while (fast != null && fast.next != null){
+            
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast){
+                isCycle = true;
+                break;
+            }
+        }
+        if (isCycle == false){
+            return null;
+        }
+        slow = head;
+        while(slow != fast){
+            slow= slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
