5b41c813b0b39f16b17c6966d2c5a6c226fd7a51
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-09-14 17:27:42 +0530

    Sort012 important question

---
+++ Sort012.java
@@ -0,0 +1,50 @@
+public class Sort012 {
+    //https://www.geeksforgeeks.org/problems/given-a-linked-list-of-0s-1s-and-2s-sort-it/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+    static NodeG segregate(NodeG head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        NodeG dummy0 = new NodeG (-1);
+        NodeG zero = dummy0;
+        
+        NodeG dummy1 = new NodeG (-1);
+        NodeG one = dummy1;
+        
+        NodeG dummy2 = new NodeG (-1);
+        NodeG two = dummy2;
+        
+        NodeG curr = head;
+        while(curr != null){
+            if (curr.data == 0){
+                zero.next = curr;
+                zero = zero.next;
+            }else if(curr.data == 1){
+                one.next = curr;
+                one = one.next;
+            }else{
+                two.next = curr;
+                two = two.next;
+            }
+            curr = curr.next;
+        }
+        zero.next = null;
+        one.next = null;
+        two.next = null;
+        
+        if (dummy0.next != null && dummy1.next != null){
+            zero.next = dummy1.next;
+            one.next = dummy2.next;
+            return dummy0.next;
+        }
+        if (dummy0.next == null && dummy1.next != null){
+            
+            one.next = dummy2.next;
+            return dummy1.next;
+        }
+        if (dummy0.next != null && dummy1.next == null){
+            zero.next = dummy2.next;
+            return dummy0.next;
+        }
+        return dummy2.next;
+    }
+}
